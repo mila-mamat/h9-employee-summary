6ae566890665e1ab57f033fa68ea4cf97a560996
mila-mamat <59339564+mila-mamat@users.noreply.github.com>
2020-04-16 19:15:00 -0400

    defaultID bug is fixed

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-const getEmployeesInfo = require("./lib/getEmployeesInfo");
 const fs = require("fs");
-
 const path = require("path");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-///Users/milaw/Desktop/02-Homework/Develop/output
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-///Users/milaw/Desktop/02-Homework/Develop/output/team.html
-
+const getEmployeesInfo = require("./lib/getEmployeesInfo");
 const render = require("./lib/htmlRenderer");
 
 async function createHTML() {
-    const employeesInfo = await getEmployeesInfo();
+  const employeesInfo = await getEmployeesInfo();
+  const html = render(employeesInfo);
 
-    // const html = render(employeesInfo);
+  //check if output folder exists, if not create one
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
 
-    // //check if output folder exists, if not create one
-    // if (!fs.existsSync(OUTPUT_DIR)) {
-    //     fs.mkdirSync(OUTPUT_DIR);
-    // }
-
-    // //create a team.html file under output folder
-    // fs.writeFile(outputPath, html, function (err) {
-    //     if (err) throw err;
-    //     console.log("Team.html is created ");
-    // });
+  //create a team.html file under output folder
+  fs.writeFile(outputPath, html, function (err) {
+    if (err) throw err;
+    console.log("Team.html is created ");
+  });
 }
 createHTML();
